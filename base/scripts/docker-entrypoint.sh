73b7c03ac9b2bd35a31c6ece5b325597a951d2f2
Aerzas <nicolas.ricklin@protonmail.com>
2020-06-07 15:54:15 +0200

    Fix entrypoint

--- base/scripts/docker-entrypoint.sh
+++ base/scripts/docker-entrypoint.sh
@@ -2,9 +2,12 @@
 set -e
 
 # Replace environment variables in template files
-envs=$(printf '${%s} ' "$(sh -c \"env | cut -d'=' -f1\")")
-find "${PHP_INI_DIR}" "${PHP_FPM_CONF_DIR}" -name '*.tmpl' \
-  -exec sh -c 'filename="$1"; envsubst "${envs}" <"${filename}" >"${filename%?????}"' \
-  -exec rm {} \;
+envs=$(printf '${%s} ' $(sh -c "env | cut -d'=' -f1"))
+find "${PHP_INI_DIR}" "${PHP_FPM_CONF_DIR}" -type f -name '*.tmpl' > /tmp/tmpl
+while IFS= read -r filename; do
+  envsubst "${envs}" <"${filename}" >"${filename%.tmpl}"
+  rm "${filename}"
+done < /tmp/tmpl
+rm /tmp/tmpl
 
 exec /usr/local/bin/docker-php-entrypoint "$@"
