34ab4b84aff068ba2145df405df780f700d845e9
Aerzas <nicolas.ricklin@protonmail.com>
2021-12-27 10:28:11 +0100

    Bump base docker images versions

--- build.sh
+++ build.sh
@@ -21,10 +21,10 @@ php_base_tag() {
 
   case ${php_version} in
   7.3)
-    echo php:7.3.31-fpm-alpine3.14
+    echo php:7.3.33-fpm-alpine3.15
     ;;
   7.4)
-    echo php:7.4.24-fpm-alpine3.14
+    echo php:7.4.27-fpm-alpine3.15
     ;;
   *)
     return 0
